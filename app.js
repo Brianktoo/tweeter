57ac7e5a1fd41e9091f4ff1e6068bcf95671599d
brianktoo <brianktoo1998@gmail.com>
2024-03-14 18:54:34 +0300

    first-tweet-commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var tweet= prompt(' Compose your tweet');
+// var tweetLenght = tweet.length;
+var tweetUnder30 = tweet.slice(0,30)
+// var rem = 100 - tweetLenght
+//  alert('you have written ' + tweetLenght +  ' characters' + 'you are remaining with '+ rem + ' characters');
+alert(tweetUnder30)
